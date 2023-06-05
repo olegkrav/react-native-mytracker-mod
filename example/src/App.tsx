dab6ce599faf03580121ad286626502c39a480e5
o.kravtsov <1qaz!QAZ>
2023-06-05 19:45:44 +0400

    fix(npm): test create npm repo

---
+++ example/src/App.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+import MyTracker from '@olegkrav/react-native-mytracker-mod';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  MyTracker.initTracker('initTracker');
+
+  // MyTracker.trackEvent('trackEvent');
+
+  // MyTracker.trackEvent('trackEventWithParams', {
+  //   event1: '1',
+  //   event2: '2',
+  // });
+  return (
+    <View style={styles.container}>
+      <Text>Result</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
